fc0bf538b8fba60546b9537428045e9699ac2d01
AethelVeritas <m.git@dv.dx.ro>
2025-09-27 20:26:31 +0300

    added arrow mechanics

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,9 @@ extends CharacterBody2D
 var speed = 100
 
 var player_state 
+#var bow_equiped = true
+var bow_reload = true
+var arrow = preload("res://scenes/arrow.tscn")
 
 func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
@@ -15,5 +18,18 @@ func _physics_process(delta):
 		
 	velocity = direction * speed
 	move_and_slide()
-
+	var mouse_pos =  get_global_mouse_position()
+	$Marker2D.look_at(mouse_pos)
+	
+	if Input.is_action_just_pressed("left_mouse") and bow_reload: #and bow_equiped :
+		#bow_equiped = false
+		var arrow_instance = arrow.instantiate()
+		arrow_instance.rotation = $Marker2D.rotation
+		arrow_instance.global_position = $Marker2D.global_position
+		add_child(arrow_instance)
+		await get_tree().create_timer(0.4).timeout
+		bow_reload = true
+ 	
 	#play_anim(direction)
+	
+	
