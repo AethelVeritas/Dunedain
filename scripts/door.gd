a646a1fc50f40cf5aeca9d40a000b83b4cd817a5
Valeriucoding <valeriucelmare1@gmail.com>
2025-09-28 03:11:16 +0300

    add not working transition

--- scripts/door.gd
+++ scripts/door.gd
@@ -3,7 +3,7 @@ extends Area2D
 
 class_name Door
 
-@export var destination_level: String
+@export var destination_level_tag: String
 @export var destination_door_tag: String
 @export var spawn_direction = "up"
 
@@ -12,5 +12,7 @@ class_name Door
 
 
 func _on_body_entered(body: Node2D) -> void:
+	print("body_enteredeww")
 	if body.is_in_group("Player"):
-		pass
+		NavigationManager.go_to_level(destination_level_tag, destination_door_tag)
+		
--- scripts/navigation_manager.gd
+++ scripts/navigation_manager.gd
@@ -5,7 +5,18 @@ const scene_root = preload("res://scenes/root.tscn")
 
 var 	spawn_door_tag
 
-func go_to_level(level_tag, door_tag	):
-	pass
-	#match level_tag:
-		#"lobby"
+func go_to_level(level_tag, destination_tag	):
+	var scene_to_load 	
+	match level_tag:
+		"introduction":
+			scene_to_load = scene_introduction
+		"root":
+			scene_to_load = scene_root
+	print(level_tag)
+	if scene_to_load != null:
+		#TransitionScreen.transition()
+		#await TransitionScreen.on_transition_finished
+		spawn_door_tag = destination_tag
+		get_tree().change_scene_to_packed(scene_to_load)
+		
+			
